5318e30b1a7ecffd4adc08e308843f943e91ed17
Gonçalo Palma <solid.goncalo@gmail.com>
2020-02-20 20:28:10 +0400

    Fixing Localizations with Builder

--- lib/src/localized_hello_widget.dart
+++ lib/src/localized_hello_widget.dart
@@ -6,19 +6,38 @@ class LocalizedHello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Localizations.override(
-          context: context,
-          delegates: [
-            const LibLocalizationsDelegate()
-          ],
-          child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          border: Border.all(color: Color(0xff75eaff), style: BorderStyle.solid, width: 6.0),
-          color: Color(0xffb1ff75),
+      context: context,
+      delegates: [const LibLocalizationsDelegate()],
+      child: Builder(
+        builder: (secondContext) => Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            border: Border.all(
+                color: Color(0xff75eaff), style: BorderStyle.solid, width: 6.0),
+            color: Color(0xffb1ff75),
+          ),
+          padding: EdgeInsets.all(18.0),
+          child: Text(
+            LibLocalizations.of(secondContext).textGreeting,
+            style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xffff7a75)),
+          ),
         ),
-        padding: EdgeInsets.all(18.0),
-        child: Text(LibLocalizations.of(context).textGreeting, style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, color: Color(0xffff7a75)),),
       ),
     );
   }
+}
+
+//ignore:unused_element
+class _GreetingText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Text(
+        LibLocalizations.of(context).textGreeting,
+        style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xffff7a75)),
+      );
 }
