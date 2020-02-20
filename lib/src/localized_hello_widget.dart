b21fe62f382472078c647bc2fb2b10a5929cf241
Gonçalo Palma <solid.goncalo@gmail.com>
2020-02-20 20:24:01 +0400

    Localizations Error - Wrong BuildContext

--- lib/src/localized_hello_widget.dart
+++ lib/src/localized_hello_widget.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:localized_hello/src/localization/lib_localizations.dart';
+import 'package:localized_hello/src/localization/lib_localizations_delegate.dart';
 
 class LocalizedHello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        border: Border.all(color: Color(0xff75eaff), style: BorderStyle.solid, width: 6.0),
-        color: Color(0xffb1ff75),
+    return Localizations.override(
+          context: context,
+          delegates: [
+            const LibLocalizationsDelegate()
+          ],
+          child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          border: Border.all(color: Color(0xff75eaff), style: BorderStyle.solid, width: 6.0),
+          color: Color(0xffb1ff75),
+        ),
+        padding: EdgeInsets.all(18.0),
+        child: Text(LibLocalizations.of(context).textGreeting, style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, color: Color(0xffff7a75)),),
       ),
-      padding: EdgeInsets.all(18.0),
-      child: Text("Bom dia!", style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, color: Color(0xffff7a75)),),
     );
   }
 }
